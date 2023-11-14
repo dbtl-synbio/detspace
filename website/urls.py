db5d536321a766701c3832c69995cc857d9fcfb9
richard2049 <133959513+richard2049@users.noreply.github.com>
2023-11-14 16:00:07 +0100

    Web server files

---
+++ website/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name="home"),
+    path('contact.html', views.contact, name="contact"),
+    path('chassis.html', views.chassis, name="chassis"),
+    path('about.html', views.about, name="about"),
+    path('index.html', views.index, name="index"),
+    path('target.html', views.target, name="target"),
+    path('effector.html', views.effector, name="effector"),
+    path('plasmid.html', views.plasmid, name="plasmid"),
+    path('specifications.html', views.specifications, name="specifications"),
+    path('<int:target_id>', views.info, name="info"),
+]
