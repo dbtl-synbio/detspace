2094e0e7723af427a30c161cc0594e565f683a26
hecmarla <hecmarla@etsii.uv.es>
2024-01-23 11:13:55 +0100

    Added url changes

--- website/views.py
+++ website/views.py
@@ -83,7 +83,7 @@ def body_viz(request):
 def detect(request, prod='27', det='0'):
     return render(request, 'index.html', {
         'prod': prod,
-        'det': det
+        'det': det,
     }
     )
 
